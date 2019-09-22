bf4d786a5800c8b0d2b38dc8836f9768b1b76616
tsontang <tsontang@jidouauto.com>
2019-09-22 23:06:13 +0800

    添加部分配置

--- src/main/java/com/tson/yd/TsonApplication.java
+++ src/main/java/com/tson/yd/TsonApplication.java
@@ -1,9 +1,11 @@
 package com.tson.yd;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.tson.yd.dao")
 public class TsonApplication {
 
     public static void main(String[] args) {
