0b84af0b68980b3ace7359df4ec71fa7d9bb3b41
tsontang <tsontang@jidouauto.com>
2019-09-22 22:38:39 +0800

    init

---
+++ src/main/java/com/tson/yd/TsonApplication.java
@@ -0,0 +1,13 @@
+package com.tson.yd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TsonApplication.class, args);
+    }
+
+}
