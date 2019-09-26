4f170783611d13a50ee23b6c369860d7625af2db
tsontang <tsontang@jidouauto.com>
2019-09-26 22:47:33 +0800

    0.0.7发布

--- src/main/java/com/tson/yd/TsonApplication.java
+++ src/main/java/com/tson/yd/TsonApplication.java
@@ -19,45 +19,45 @@ public class TsonApplication {
     public static void main(String[] args) {
         SpringApplication.run(TsonApplication.class, args);
     }
-
-    @Value("${http.port}")
-    Integer httpPort;
-
-    // 正常启用的https端口 如443
-    @Value("${server.port}")
-    Integer httpsPort;
-
-    // springboot2 写法
-    @Bean
-    @ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
-    public TomcatServletWebServerFactory servletContainer() {
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint constraint = new SecurityConstraint();
-                constraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                constraint.addCollection(collection);
-                context.addConstraint(constraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-
-    @Bean
-    @ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
-    public Connector httpConnector() {
-        System.out.println("启用http转https协议，http端口：" + this.httpPort + "，https端口：" + this.httpsPort);
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        // Connector监听的http的端口号
-        connector.setPort(httpPort);
-        connector.setSecure(false);
-        // 监听到http的端口号后转向到的https的端口号
-        connector.setRedirectPort(httpsPort);
-        return connector;
-    }
+//
+//    @Value("${http.port}")
+//    Integer httpPort;
+//
+//    // 正常启用的https端口 如443
+//    @Value("${server.port}")
+//    Integer httpsPort;
+//
+//    // springboot2 写法
+//    @Bean
+//    @ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
+//    public TomcatServletWebServerFactory servletContainer() {
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint constraint = new SecurityConstraint();
+//                constraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//                constraint.addCollection(collection);
+//                context.addConstraint(constraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    @ConditionalOnProperty(name = "condition.http2https", havingValue = "true", matchIfMissing = false)
+//    public Connector httpConnector() {
+//        System.out.println("启用http转https协议，http端口：" + this.httpPort + "，https端口：" + this.httpsPort);
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        // Connector监听的http的端口号
+//        connector.setPort(httpPort);
+//        connector.setSecure(false);
+//        // 监听到http的端口号后转向到的https的端口号
+//        connector.setRedirectPort(httpsPort);
+//        return connector;
+//    }
 
 }
