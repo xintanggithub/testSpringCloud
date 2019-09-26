599f0d6f09124df052cdaa2aa49168e62b4ce78a
tsontang <tsontang@jidouauto.com>
2019-09-26 14:06:35 +0800

    待添加登录

--- src/main/java/com/tson/yd/dao/LoginDao.java
+++ src/main/java/com/tson/yd/dao/LoginDao.java
@@ -11,5 +11,32 @@ public interface LoginDao {
      * @return 账号信息
      */
     LoginEntity queryLogin(String userCode);
-//todo 查询(需要整理 注册、登陆、注销逻辑)
+
+    /**
+     * 更新密码
+     *
+     * @param password 密码
+     */
+    void updatePassword(String password);
+
+    /**
+     * 更新用户注销状态
+     *
+     * @param register 注册 状态 1正常  0注销
+     */
+    void updateRegister(String register);
+
+    /**
+     * 更新账号数据
+     * @param password 密码
+     * @param register 注册状态 1正常 0注销
+     */
+    void updateLogin(String password, String register);
+
+    /**
+     * 新增
+     * @param loginEntity 账号数据
+     */
+    void insertLogin(LoginEntity loginEntity);
+
 }
