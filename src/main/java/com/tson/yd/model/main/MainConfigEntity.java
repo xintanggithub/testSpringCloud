00f2da6bef0a601ef6f8ef2fa6554495d26dd9ce
tsontang <tsontang@jidouauto.com>
2019-10-23 10:15:17 +0800

    首页配置参数添加按钮

--- src/main/java/com/tson/yd/model/main/MainConfigEntity.java
+++ src/main/java/com/tson/yd/model/main/MainConfigEntity.java
@@ -30,6 +30,87 @@ public class MainConfigEntity {
     @ApiModelProperty(value = "配置8")
     private String content8;
 
+    @ApiModelProperty(value = "按钮1")
+    private String img1;
+    @ApiModelProperty(value = "按钮2")
+    private String img2;
+    @ApiModelProperty(value = "按钮3")
+    private String img3;
+    @ApiModelProperty(value = "按钮4")
+    private String img4;
+    @ApiModelProperty(value = "按钮5")
+    private String img5;
+    @ApiModelProperty(value = "按钮6")
+    private String img6;
+    @ApiModelProperty(value = "按钮7")
+    private String img7;
+    @ApiModelProperty(value = "按钮8")
+    private String img8;
+
+    public String getImg1() {
+        return img1;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public String getImg2() {
+        return img2;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
+    }
+
+    public String getImg3() {
+        return img3;
+    }
+
+    public void setImg3(String img3) {
+        this.img3 = img3;
+    }
+
+    public String getImg4() {
+        return img4;
+    }
+
+    public void setImg4(String img4) {
+        this.img4 = img4;
+    }
+
+    public String getImg5() {
+        return img5;
+    }
+
+    public void setImg5(String img5) {
+        this.img5 = img5;
+    }
+
+    public String getImg6() {
+        return img6;
+    }
+
+    public void setImg6(String img6) {
+        this.img6 = img6;
+    }
+
+    public String getImg7() {
+        return img7;
+    }
+
+    public void setImg7(String img7) {
+        this.img7 = img7;
+    }
+
+    public String getImg8() {
+        return img8;
+    }
+
+    public void setImg8(String img8) {
+        this.img8 = img8;
+    }
+
     public String getContent1() {
         return content1;
     }
